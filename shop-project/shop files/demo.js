37e04ec266e6524f6e155a3f04982847d71279ee
Alex Nazaretyan <alex.nazaretyan@abv.bg>
2021-06-11 13:42:36 +0300

    adding js popUp message

--- shop-project/shop files/demo.js
+++ shop-project/shop files/demo.js
@@ -1,31 +1,6 @@
-const inputOne = document.querySelector('#input-one');
-const inputTwo = document.querySelector('#input-two');
+let char = 'some text@';
 
-inputOne.addEventListener('focus', ()=>{
-    inputOne.style.cssText=`
-        background-color: rgb(108, 108, 218);
-    `;
-    inputOne.value='I am focused'
-});
-inputOne.addEventListener('blur',()=>{
-    inputOne.style.cssText=`
-        background-color:white;
-    `;
-    inputOne.value='';
-})
+for(let i = 0; i<char.length;i++){
+    console.log(char.indexOf(`@ ${i}`));
+}
 
-inputTwo.addEventListener('focus', ()=>{
-    inputTwo.style.cssText=`
-        background-color:rgb(108,108,218);
-    `;
-    inputTwo.value='I am focused'
-})
-inputTwo.addEventListener('blur',()=>{
-    inputTwo.style.cssText=`
-        background-color:white;
-    `;
-    inputTwo.value='';
-})
-cadetblue
-cornflowerblue
-lightsteelblue
--- shop-project/shop files/js/log-in.js
+++ shop-project/shop files/js/log-in.js
@@ -1,10 +1,45 @@
-function solve(){
+function emailValidation() {
     const validUserName = 'deJav1u@abv.bg';
     const logInForm = document.forms.logIn;
     const submitButton = document.querySelector('.btn')
-    const userNameValue = logInForm.elements.email.value;
-    console.log(submitButton);
+    submitButton.addEventListener('click', () => {
+        if (logInForm.elements.email.value === validUserName) {
+            const greenCheck = document.querySelector('.green-check');
+            const redCheck = document.querySelector('.red-check');
+            greenCheck.style.cssText = `
+                display:inline-block;
+                color:green;
+                padding-left:10px;
+            `;
+            redCheck.style.cssText = `
+                display:none;
+            `;
+
+        } else {
+            const greenCheck = document.querySelector('.green-check');
+            const redCheck = document.querySelector('.red-check');
+            const emailText = document.querySelector('.email-text-correct');
+            emailText.style.cssText = `
+                color:green; 
+            `;
+            redCheck.style.cssText = `
+                display:inline-block;
+                color:red;
+                padding-left:10px;
+            `;
+            greenCheck.style.cssText = `
+                display:none;
+            `;
+        }
+        let element =  logInForm.elements.email.value = logInForm.elements.email.value;
+        element = 'some text'
+        
+       
+        
+    })
     
-  
+
+
+
 }
-solve();
+emailValidation();
